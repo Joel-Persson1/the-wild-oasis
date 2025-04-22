5918b6085f9d2a39a5bbd4f97ee9670720eadd66
Joel-Persson1 <jolle.ov@gmail.com>
2025-04-22 16:54:57 +0200

    added: creating new cabin, form errors, uploading images

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://xwmbmvmfyxfomalwylij.supabase.co";
+export const supabaseUrl = "https://xwmbmvmfyxfomalwylij.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh3bWJtdm1meXhmb21hbHd5bGlqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUyMzcwNjUsImV4cCI6MjA2MDgxMzA2NX0.aT9OGXXvpwfOyNb_GTIC5RlaioyOJ44Bdm7-mij8XCE";
 const supabase = createClient(supabaseUrl, supabaseKey);

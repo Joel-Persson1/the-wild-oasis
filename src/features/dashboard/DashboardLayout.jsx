351c8965faca1677edc6793fb3f17d667909d29a
Joel-Persson1 <jolle.ov@gmail.com>
2025-04-27 17:52:55 +0200

    Added a line chart to the dashboard page

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -4,6 +4,7 @@ import Spinner from "../../ui/Spinner";
 import { useRecentStays } from "./useRecentStays";
 import Stats from "./Stats";
 import { useCabins } from "../cabins/useCabins";
+import SalesChart from "./SalesChart";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -14,17 +15,10 @@ const StyledDashboardLayout = styled.div`
 
 function DashboardLayout() {
   const { isLoading: isLoading1, bookings } = useRecentBookings();
-  const {
-    stays,
-    confirmedStays,
-    osLoading: isLoading2,
-    numDays,
-  } = useRecentStays();
-  const { cabins, isLoading: isLoading3 } = useCabins();
+  const { isLoading: isLoading2, confirmedStays, numDays } = useRecentStays();
+  const { isLoading: isLoading3, cabins } = useCabins();
 
   if (isLoading1 || isLoading2 || isLoading3) return <Spinner />;
-
-  console.log(bookings);
 
   return (
     <StyledDashboardLayout>
@@ -36,7 +30,7 @@ function DashboardLayout() {
       />
       <div>Today's activity</div>
       <div>Chart stay durations</div>
-      <div>Chart sales</div>
+      <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   );
 }
